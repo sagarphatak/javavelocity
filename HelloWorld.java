c781db0927862355acf35a5d18398b9aa9f73d92
sagarphatak <51190769+sagarphatak@users.noreply.github.com>
2019-05-31 17:35:54 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.velocity;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello velocity");
+
+	}
+
+}
