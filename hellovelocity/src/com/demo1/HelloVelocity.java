46591ae277451243a781a9d7c933dd4bc83c9646
sagar <sagar@sagar-PC>
2019-05-31 17:29:24 +0530

    first commit
    uploading all files

---
+++ hellovelocity/src/com/demo1/HelloVelocity.java
@@ -0,0 +1,10 @@
+package com.demo1;
+
+public class HelloVelocity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello velocity");
+	}
+
+}
